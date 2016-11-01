18ae744b90b498bd67d506052fcfde90d84e3675
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-11-01 18:29:58 -0200

    Adding about view

--- NeutoniumDemo/App_Start/RoutingConfiguration.cs
+++ NeutoniumDemo/App_Start/RoutingConfiguration.cs
@@ -10,7 +10,7 @@ namespace NeutoniumDemo.App_Start
             var router = new ConventionRouter(builder, @"View\{0}\dist\index.HTML");
             //router.Register<Page1ViewModel>();
             //router.Register<Page2ViewModel>();
-            //router.Register<AboutViewModel>();
+            router.Register<AboutViewModel>();
         }
     }
 }
---
+++ NeutoniumDemo/View/About/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Related Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Vue Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Vue Forum</a></li>
+      <li><a href="https://github.com/David-Desmaisons/Neutronium" target="_blank">Neutronium</a></li>
+      <li><a href="https://github.com/David-Desmaisons/neutronium-vue" target="_blank">Neutronium Vue template</a></li>
+      <li><a href="https://github.com/David-Desmaisons/NeutoniumDemo" target="_blank">Neutronium Vue application demo</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const props={
+  viewModel: Object,
+};
+
+export default {
+  name: 'app',
+  props,
+  data () {
+    return this.viewModel
+  }
+}
+</script>
+
+<style>
+#main {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  height: 300px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
